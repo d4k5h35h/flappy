f0f1cc26c2882d2e00a8729218581426b0989e16
DAKSHESH CHITTOORI <64137511+d4k5h35h@users.noreply.github.com>
2020-05-19 22:47:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var bird;
+var pipes = [];
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  bird = new Bird();
+  pipes.push(new Pipe());
+}
+
+function draw() {
+  background(0);
+  for (var i = pipes.length - 1; i >= 0; i--) {
+    pipes[i].show();
+    pipes[i].update();
+    if (pipes[i].offscreen()) {
+      pipes.splice(i, 1);
+    }
+    
+  if (pipes[i].hits(bird)){
+    console.log("hit bad");
+  }
+  }
+
+
+  bird.show();
+  bird.update();
+
+  if (frameCount % 100 == 0) {
+    pipes.push(new Pipe());
+  }
+
+}
+
+function keyPressed() {
+  if (key == ' ') {
+    bird.up();
+  }
+}
